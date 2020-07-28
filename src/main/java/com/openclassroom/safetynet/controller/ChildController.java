fc23062839b068bab500a7752acd080cb7b3dc80
marwa sahraoui <marwasahrawi1988@gmail.com>
2020-07-28 16:19:52 +0200

    ChildController commit

---
+++ src/main/java/com/openclassroom/safetynet/controller/ChildController.java
@@ -0,0 +1,31 @@
+package com.openclassroom.safetynet.controller;
+
+
+import com.openclassroom.safetynet.model.Person;
+import com.openclassroom.safetynet.service.ChildService;
+import com.openclassroom.safetynet.service.JsonDataStructureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+
+@RestController
+public class ChildController {
+
+    @Autowired
+    JsonDataStructureService jsonDataStructureService;
+    @Autowired
+    ChildService childService;
+
+    @GetMapping("/childAlert")
+    public List<Person> children(@RequestParam("address") String address) throws IOException {
+        return childService.childrenPerAddress(address) ;
+    }
+
+}
+
+
+
+
---
+++ src/main/java/com/openclassroom/safetynet/service/ChildService.java
@@ -0,0 +1,60 @@
+package com.openclassroom.safetynet.service;
+
+import com.openclassroom.safetynet.model.Medicalrecord;
+import com.openclassroom.safetynet.model.Person;
+import com.openclassroom.safetynet.utils.JsonDataStructure;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.time.temporal.ChronoUnit.YEARS;
+
+
+@Service
+public class ChildService {
+
+    @Autowired
+    private JsonDataStructureService jsonDataStructureService;
+
+    public List<Person> childrenPerAddress(@RequestParam("address") String address) throws IOException {
+
+        JsonDataStructure jsonDataStructure = jsonDataStructureService.getJsonDataStructure();
+
+        List<Person> personAdress = new ArrayList<>();
+        List<Person> y = new ArrayList<>();
+
+        for (Person pe : jsonDataStructure.getPersons()) {
+            if (pe.getAddress().equals(address)) {
+                personAdress.add(pe);
+            }
+        }
+
+
+        for (Medicalrecord x : jsonDataStructure.getMedicalrecords()) {
+            for (Person p : personAdress) {
+                if (x.getFirstName().equals(p.getFirstName())) {
+
+                    String date = x.getBirthdate();
+
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+                    LocalDate dateTime = LocalDate.parse(date, formatter);
+
+
+                    if ((YEARS.between(dateTime, LocalDate.now()) <= 18)) {
+                        y.add(p);
+                    }
+                }
+
+            }
+        }
+
+        return y;
+    }
+
+}
