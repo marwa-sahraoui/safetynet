9ad36ba7ec6babba216d49bcb65e40f8e0b4298c
marwa sahraoui <marwasahrawi1988@gmail.com>
2020-07-30 23:34:17 +0200

    phoneAlert

---
+++ src/main/java/com/openclassroom/safetynet/controller/PhoneAlertController.java
@@ -0,0 +1,24 @@
+package com.openclassroom.safetynet.controller;
+
+
+import com.openclassroom.safetynet.service.FirestationService;
+import com.openclassroom.safetynet.service.PhoneNumberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PhoneAlertController {
+
+    @Autowired
+    PhoneNumberService phoneNumberServic;
+
+    @GetMapping("/phoneAlert")
+    public List<String> getPhoneNumber(@RequestParam("firestation") Integer firestation){
+        return phoneNumberServic.getPhoneNumber(firestation);
+    }
+
+}
---
+++ src/main/java/com/openclassroom/safetynet/service/PhoneNumberService.java
@@ -0,0 +1,44 @@
+package com.openclassroom.safetynet.service;
+
+
+import com.openclassroom.safetynet.model.Firestation;
+import com.openclassroom.safetynet.model.Person;
+import com.openclassroom.safetynet.model.ResponsePersonWithStatistic;
+import com.openclassroom.safetynet.utils.JsonDataStructure;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PhoneNumberService {
+
+    @Autowired
+
+    private JsonDataStructureService jsonDataStructureService;
+
+    public List<String> getPhoneNumber(@RequestParam("firestation") Integer firestation) {
+        List<Firestation> firestations = new ArrayList<>();
+
+        List<String> phone = new ArrayList<>();
+        JsonDataStructure jsonDataStructure = jsonDataStructureService.getJsonDataStructure();
+
+        for (Firestation f : jsonDataStructure.getFirestations()) {
+            if (f.getStation().equals(String.valueOf(firestation))) {
+                firestations.add(f);
+            }
+        }
+
+        for (Firestation firestationss : firestations) {
+            for (Person p : jsonDataStructure.getPersons()) {
+                if (firestationss.getAddress().equals(p.getAddress())) {
+                    phone.add(p.getPhone());
+                }
+            }
+        }
+
+        return phone;
+    }
+}
