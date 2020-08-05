993b7b55c98393f85d860a6d3d23293446ccb250
marwa sahraoui <marwasahrawi1988@gmail.com>
2020-08-05 16:14:51 +0200

    PersonInfo controller

---
+++ src/main/java/com/openclassroom/safetynet/controller/PersonInfoController.java
@@ -0,0 +1,25 @@
+package com.openclassroom.safetynet.controller;
+
+import com.openclassroom.safetynet.model.Fire;
+import com.openclassroom.safetynet.model.PersonInfo;
+import com.openclassroom.safetynet.service.FirestationService;
+import com.openclassroom.safetynet.service.PersonInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+
+@RestController
+public class PersonInfoController {
+
+    @Autowired
+    PersonInfoService personInfoService;
+
+    @GetMapping("/personInfo")
+    public PersonInfo personInfo(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName) throws IOException {
+        return personInfoService.personInfo(firstName,lastName);
+   }
+}
---
+++ src/main/java/com/openclassroom/safetynet/service/PersonInfoService.java
@@ -0,0 +1,56 @@
+package com.openclassroom.safetynet.service;
+
+
+import com.openclassroom.safetynet.model.Medicalrecord;
+import com.openclassroom.safetynet.model.Person;
+import com.openclassroom.safetynet.model.PersonInfo;
+import com.openclassroom.safetynet.utils.JsonDataStructure;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.time.temporal.ChronoUnit.YEARS;
+
+@Service
+public class PersonInfoService {
+    @Autowired
+    private JsonDataStructureService jsonDataStructureService;
+
+    public PersonInfo personInfo(String firstName, String lastName) {
+
+        JsonDataStructure jsonDataStructure = jsonDataStructureService.getJsonDataStructure();
+
+        for (Medicalrecord m : jsonDataStructure.getMedicalrecords()) {
+            for (Person person : jsonDataStructure.getPersons()) {
+                if (person.getFirstName().equals(m.getFirstName()) && person.getLastName().equals(m.getLastName())
+                        && (person.getFirstName()).equals(firstName) && (person.getLastName().equals(lastName))) {
+
+                    String date = m.getBirthdate();
+
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+                    LocalDate dateTime = LocalDate.parse(date, formatter);
+
+
+                    YEARS.between(dateTime, LocalDate.now());
+                    int age = (int) YEARS.between(dateTime, LocalDate.now());
+                    m.getMedications();
+                    m.getAllergies();
+
+                    PersonInfo personInfo = new PersonInfo(person.getFirstName(), person.getLastName(), person.getAddress()
+                            , age, person.getEmail(), m.getMedications(), m.getAllergies());
+                    System.out.println(personInfo);
+                    return personInfo;
+                }
+
+            }
+
+        }
+        return null;
+
+    }
+}
